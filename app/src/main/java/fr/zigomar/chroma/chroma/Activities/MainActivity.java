f43b2f5535c83b0c05f19db818c45f4705941748
ro-olivier <ro.olivier@gmail.com>
2018-03-12 23:13:20 +0100

    New option to export data only between selected dates. Dates are provided by the user using a Fragment.

--- app/src/main/java/fr/zigomar/chroma/chroma/Activities/MainActivity.java
+++ app/src/main/java/fr/zigomar/chroma/chroma/Activities/MainActivity.java
@@ -1,6 +1,7 @@
 package fr.zigomar.chroma.chroma.Activities;
 
 import android.Manifest;
+import android.app.DialogFragment;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Environment;
@@ -29,17 +30,22 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.Locale;
 import java.util.Objects;
 
+import fr.zigomar.chroma.chroma.Fragments.ExportDateFragment;
 import fr.zigomar.chroma.chroma.R;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String CURRENT_DATE = "com.example.chroma.current_date";
-    private static final int CHROMA_WRITE_EXTERNAL_STORAGE_PERMISSION = 100;
+    private static final int CHROMA_WRITE_EXTERNAL_STORAGE_PERMISSION_FULL_EXPORT = 100;
+    private static final int CHROMA_WRITE_EXTERNAL_STORAGE_PERMISSION_DATE_EXPORT = 101;
 
     private Date currentDate = new Date();
 
@@ -178,25 +184,42 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.action_export:
-
+            case R.id.action_export_all:
+                Log.i("CHROMA", "User request full export");
                 if (ContextCompat.checkSelfPermission(this,
                         Manifest.permission.WRITE_EXTERNAL_STORAGE)
                         != PackageManager.PERMISSION_GRANTED) {
-
+                    Log.i("CHROMA", "Request code : CHROMA_WRITE_EXTERNAL_STORAGE_PERMISSION_FULL_EXPORT");
                     ActivityCompat.requestPermissions(
                             this,
                             new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                            CHROMA_WRITE_EXTERNAL_STORAGE_PERMISSION);
+                            CHROMA_WRITE_EXTERNAL_STORAGE_PERMISSION_FULL_EXPORT);
                 } else {
+                    Log.i("CHROMA", "Permission already granted, proceeding with export()");
                     export();
                 }
 
                 return true;
 
+            case R.id.action_export:
+                Log.i("CHROMA", "User request date export");
+                if (ContextCompat.checkSelfPermission(this,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    Log.i("CHROMA", "Requesting permission WRITE_EXTERNAL_STORAGE");
+                    Log.i("CHROMA", "Request code : CHROMA_WRITE_EXTERNAL_STORAGE_PERMISSION_DATE_EXPORT");
+                    ActivityCompat.requestPermissions(
+                            this,
+                            new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                            CHROMA_WRITE_EXTERNAL_STORAGE_PERMISSION_DATE_EXPORT);
+                } else {
+                    Log.i("CHROMA", "Permission already granted, proceeding with getDateAndExport()");
+                    getDateAndExport();
+                }
+
+                return true;
+
             default:
-                // If we got here, the user's action was not recognized.
-                // Invoke the superclass to handle it.
                 return super.onOptionsItemSelected(item);
 
         }
@@ -207,38 +230,162 @@ public class MainActivity extends AppCompatActivity {
                                            @NonNull String permissions[],
                                            @NonNull int[] grantResults) {
         switch (requestCode) {
-            case CHROMA_WRITE_EXTERNAL_STORAGE_PERMISSION: {
+            case CHROMA_WRITE_EXTERNAL_STORAGE_PERMISSION_FULL_EXPORT: {
 
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
+                    Log.i("CHROMA", "Received permission CHROMA_WRITE_EXTERNAL_STORAGE_PERMISSION_FULL_EXPORT");
                     export();
 
                 } else {
 
-                    Log.i("CHROMA", "Permission refused, aborting.");
+                    Log.i("CHROMA", "Permission refused, aborting full export.");
                 }
+
+                break;
+            }
+
+            case CHROMA_WRITE_EXTERNAL_STORAGE_PERMISSION_DATE_EXPORT: {
+
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.i("CHROMA", "Received permission CHROMA_WRITE_EXTERNAL_STORAGE_PERMISSION_DATE_EXPORT");
+                    getDateAndExport();
+
+                } else {
+
+                    Log.i("CHROMA", "Permission refused, aborting full export.");
+                }
+
+                break;
+            }
+        }
+    }
+
+    private void getDateAndExport() {
+        ArrayList<String> available_dates = new ArrayList<>();
+
+        for (File f: getFilesDir().listFiles()) {
+            if (f.isFile()) {
+                if (!Objects.equals(f.getName(), "openbooks.json")) {
+                    Log.i("CHROMA", f.getName());
+                    available_dates.add(f.getName().substring(0,10));
+                }
+            }
+        }
+
+        String[] available_dates_array = new String[available_dates.size()];
+        Collections.sort(available_dates, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+
+                int val1 = Integer.parseInt(o1.split("-")[0] + o1.split("-")[1] + o1.split("-")[2]);
+                int val2 = Integer.parseInt(o2.split("-")[0] + o2.split("-")[1] + o2.split("-")[2]);
+
+                if (val1 < val2) {
+                    return -1;
+                } else if (val1 == val2) {
+                    return 0;
+                } else {
+                    return 1;
+                }
+            }
+        });
+
+        DialogFragment newFragment = ExportDateFragment.newInstance(available_dates.toArray( available_dates_array ));
+        newFragment.show(getFragmentManager(), "dialog");
+    }
+
+    public void exportOnReceiveExportDates(String beginDate, String endDate) {
+
+        Log.i("CHROMA", "exportOnReceiveExportDates was called");
+
+        int beginDateInt = Integer.parseInt(beginDate.split("-")[0] + beginDate.split("-")[1] + beginDate.split("-")[2]);
+        int endDateInt = Integer.parseInt(endDate.split("-")[0] + endDate.split("-")[1] + endDate.split("-")[2]);
+
+        JSONArray data = new JSONArray();
+        for (File f: getFilesDir().listFiles()) {
+            if (f.isFile()) {
+                String filename = f.getName();
+                String filenameIntStr = filename.substring(0, 10);
+
+                if (!Objects.equals(filename, "openbooks.json")) {
+                    Log.i("CHROMA", "Processing file date : " + filename);
+                    int filenameInt = Integer.parseInt(filenameIntStr.split("-")[0] + filenameIntStr.split("-")[1] + filenameIntStr.split("-")[2]);
+
+                    if (filenameInt >= beginDateInt && filenameInt <= endDateInt) {
+                        Log.i("CHROMA", "Reading data from file : " + filename);
+                        try {
+                            InputStream is = getApplicationContext().openFileInput(filename);
+                            int size = is.available();
+                            byte[] buffer = new byte[size];
+                            int byte_read = is.read(buffer);
+                            if (byte_read != size) {
+                                Log.i("CHROMA", "Did not read the complete file, or something else went wrong");
+                            }
+                            is.close();
+                            JSONObject temp = new JSONObject(new String(buffer, "UTF-8"));
+                            temp.put("date", filename.substring(0, 10));
+                            data.put(temp);
+                        } catch (IOException | JSONException e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        Log.i("CHROMA", "Dismissing file date : " + filename);
+                    }
+                }
+            }
+        }
+
+        String exportFilename;
+        if (beginDateInt != endDateInt) {
+            exportFilename = "chroma_export_" + beginDate + "_" + endDate + ".json";
+        } else {
+            exportFilename = "chroma_export_" + beginDate + ".json";
+        }
+
+        Log.i("CHROMA", "Writing to : " + exportFilename);
+
+        File exportFile = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DOCUMENTS), exportFilename);
+
+        FileOutputStream stream = null;
+        try {
+            stream = new FileOutputStream(exportFile);
+            stream.write(data.toString().getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (stream != null) {
+                    stream.close();
+                    Toast.makeText(getApplicationContext(), R.string.ExportOK, Toast.LENGTH_SHORT).show();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
     }
 
     private void export() {
 
+        Log.i("CHROMA", "export was called");
 
         JSONArray data = new JSONArray();
         for (File f: getFilesDir().listFiles()) {
             if (f.isFile()) {
-                if (!Objects.equals(f.getName(), "openbooks.json")) {
-                    Log.i("CHROMA", "Processing " + f.getName());
+                String filename = f.getName();
+                if (!Objects.equals(filename, "openbooks.json")) {
+                    Log.i("CHROMA", "Processing " + filename);
                     try {
-                        InputStream is = getApplicationContext().openFileInput(f.getName());
+                        InputStream is = getApplicationContext().openFileInput(filename);
                         int size = is.available();
                         byte[] buffer = new byte[size];
                         int byte_read = is.read(buffer);
                         if (byte_read != size) { Log.i("CHROMA", "Did not read the complete file, or something else went wrong"); }
                         is.close();
                         JSONObject temp = new JSONObject(new String(buffer, "UTF-8"));
-                        temp.put("date", f.getName().substring(0,10));
+                        temp.put("date", filename.substring(0,10));
                         data.put(temp);
                     } catch (IOException | JSONException e) {
                         e.printStackTrace();
@@ -248,7 +395,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         File exportFile = new File(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_DOCUMENTS), "chroma_export.json");
+                Environment.DIRECTORY_DOCUMENTS), "chroma_full_export.json");
+
+        Log.i("CHROMA", "Writing to : chroma_full_export.json");
+
         FileOutputStream stream = null;
         try {
             stream = new FileOutputStream(exportFile);
